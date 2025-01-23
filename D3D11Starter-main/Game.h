82dbeb4290111e2891858f206397b08e55953d76
wcd6573 <wcd6573@rit.edu>
2025-01-23 10:08:53 -0500

    Initial commit for GGP2 repo.

---
+++ D3D11Starter-main/Game.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <d3d11.h>
+#include <wrl/client.h>
+
+class Game
+{
+public:
+	// Basic OOP setup
+	Game() = default;
+	~Game();
+	Game(const Game&) = delete; // Remove copy constructor
+	Game& operator=(const Game&) = delete; // Remove copy-assignment operator
+
+	// Primary functions
+	void Initialize();
+	void Update(float deltaTime, float totalTime);
+	void Draw(float deltaTime, float totalTime);
+	void OnResize();
+
+private:
+
+	// Initialization helper methods - feel free to customize, combine, remove, etc.
+	void LoadShaders();
+	void CreateGeometry();
+
+	// Note the usage of ComPtr below
+	//  - This is a smart pointer for objects that abide by the
+	//     Component Object Model, which DirectX objects do
+	//  - More info here: https://github.com/Microsoft/DirectXTK/wiki/ComPtr
+
+	// Buffers to hold actual geometry data
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
+
+	// Shaders and shader-related constructs
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader;
+	Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout;
+};
+
---
+++ D3D11Starter-main/README.md
@@ -0,0 +1,2 @@
+# D3D1Starter
+Starter code for a D3D11-based project
