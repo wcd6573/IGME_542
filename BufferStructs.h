71987f2e7d83f6bca6d7dd7f5313ca6f1ecee3c3
wcd6573 <wcd6573@rit.edu>
2025-02-02 00:25:38 -0500

    Made a BufferStructs header file.

---
+++ BufferStructs.h
@@ -0,0 +1,17 @@
+/*
+William Duprey
+2/2/25
+Buffer Structs Definitions
+*/
+
+#pragma once
+#include <DirectXMath.h>
+
+// --------------------------------------------------------
+// Struct that maps exactly to the vertex shader's cbuffer.
+// --------------------------------------------------------
+struct VertexShaderExternalData {
+	DirectX::XMFLOAT4X4 World;
+	DirectX::XMFLOAT4X4 View;
+	DirectX::XMFLOAT4X4 Projection;
+};
