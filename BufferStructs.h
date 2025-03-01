85451c1b956087ebcdd2329f8b3b7ec160f2a837
wcd6573 <wcd6573@rit.edu>
2025-03-01 01:07:33 -0500

    Added RaytracingSceneData struct to BufferStructs.h

--- BufferStructs.h
+++ BufferStructs.h
@@ -30,3 +30,13 @@ struct PixelShaderExternalData
 	int lightCount;
 	Light lights[MAX_LIGHTS];
 };
+
+// --------------------------------------------------------
+// Overall scene data for raytracing.
+// --------------------------------------------------------
+struct RaytracingSceneData 
+{
+	DirectX::XMFLOAT4X4 inverseViewProjection;
+	DirectX::XMFLOAT3 cameraPosition;
+	float pad;
+};
